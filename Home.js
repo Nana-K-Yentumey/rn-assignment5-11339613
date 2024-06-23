35e32eb314bf97d4692eea44837de88e9d3c9fca
death-inspires-me <nana.esseney@gmail.com>
2024-06-23 11:38:50 +0000

    Finished welcome section

--- Home.js
+++ Home.js
@@ -1,10 +1,20 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Home!</Text>
+      <View style={styles.welcomeSect}>
+        <Image style={styles.profile} source={require('./assets/profile.png')}/>
+        <View style={styles.nameSect}>
+          <Text style={styles.welcomeText}>Welcome back,</Text>
+          <Text style={styles.name}>Eric Atsu</Text>
+        </View>
+        <Ionicons style={styles.search} name="search" size={30} color="grey" />
+      </View>
+      <Image style={styles.card} source={require('./assets/Card.png')}/>
     </View>
   );
 }
@@ -14,7 +24,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+
+  welcomeSect: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    margin: 10,
+    marginTop: 50,
+  
+  },
+
+  nameSect: {
+    marginLeft: 20,
+  },
+
+  welcomeText: {
+    fontSize: 17,
+    color: 'grey',
+
+  },
+
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#1e1e2d',
+  },
+
+  search: {
+    marginLeft: 100,
+    width: 40,
+    height: 40,
+  },
+
+  profile: {
+    width: 60,
+    height: 60,
   },
 });
